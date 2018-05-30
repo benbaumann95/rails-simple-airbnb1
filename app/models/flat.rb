6980a087050b0c469ee482a5ec2d3ed816376c39
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-05-30 11:16:14 +0100

    model picture change

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,7 +3,8 @@ class Flat < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :description, presence: true
+  validates :description, presence:
+  validates :pictureUrl, presence: true
   validates :number_of_guests, presence: true
   validates :price_per_night, presence: true
 
